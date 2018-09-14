b01c0ae9c93d6c80b4915c3777fc27327d4d6868
Eldho <eldho@live.in>
2018-09-14 10:04:09 +0530

    Added dependencies Added Note Entity

---
+++ app/src/main/java/in/eldhopj/architecturecomponents_mvvmmodelsample/NoteEntity.java
@@ -0,0 +1,47 @@
+package in.eldhopj.architecturecomponents_mvvmmodelsample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.PrimaryKey;
+
+public class NoteEntity {
+
+    /**
+     * Room will autoGenerate columns for these fields
+     */
+    @PrimaryKey(autoGenerate = true)
+    //Room will auto generate the ID , we don't wanna worry about it
+    private int id;
+    private String title;
+    private String description;
+    @ColumnInfo(name = "priority_number") //column name will be priority_number
+    private int priority;
+
+    //@Ignore -> for ignoring
+
+    public NoteEntity(String title, String description, int priority) { //NOTE : We don't wanna pass ID , Since its auto generating
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //NOTE : If a variable is not in the constructor it must have a setter method
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
