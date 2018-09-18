1c01ef96f1f839b29cd7deeca62d2a5a7ef7e1d0
Eldho <eldho@live.in>
2018-09-18 11:38:41 +0530

    Added repository

--- app/src/main/java/in/eldhopj/architecturecomponents_mvvmmodelsample/NoteDatabase.java
+++ app/src/main/java/in/eldhopj/architecturecomponents_mvvmmodelsample/NoteDatabase.java
@@ -1,9 +1,12 @@
 package in.eldhopj.architecturecomponents_mvvmmodelsample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 /**Connect all different parts*/
 @Database(entities = {NoteEntity.class}, version = 1, exportSchema = false)
@@ -17,10 +20,38 @@ public abstract class NoteDatabase extends RoomDatabase {
         if (instance == null) { // if the AppDatabase instance is not created ie, null create one
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, NoteDatabase.DATABASE_NAME)
-                   // .allowMainThreadQueries() // WARNING : Queries should be done in a separate thread to avoid locking the UI, We will allow this ONLY TEMPORALLY to see that our DB is working
+                    // .allowMainThreadQueries() // WARNING : Queries should be done in a separate thread to avoid locking the UI, We will allow this ONLY TEMPORALLY to see that our DB is working
                     .fallbackToDestructiveMigration() //WARNING : Deletes existing Db while migrating into another version
+                    .addCallback(roomCallback)// NOTE : Only for creating pre-filled values in the DB
                     .build(); // returns instance of this DB
         }
         return instance;
+    }
+
+
+    //---------------------Populating Dummy values-----------------------------//
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) { //onCreate calls only when DB is created
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    //AsyncTask for populating dummy values into DB
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new NoteEntity("Title 1", "Description 1", 1));
+            noteDao.insert(new NoteEntity("Title 2", "Description 2", 2));
+            noteDao.insert(new NoteEntity("Title 3", "Description 3", 3));
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/in/eldhopj/architecturecomponents_mvvmmodelsample/NoteRepository.java
@@ -0,0 +1,103 @@
+package in.eldhopj.architecturecomponents_mvvmmodelsample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+/**
+ * Repository is a way to abstract all the database operations including in n/w and provides a clean API to the rest of the app
+ * */
+public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<NoteEntity>> allNotes;
+
+    // Application is a subClass of context; application context stays same throughout the lifecycle of the app so we pass application context for something live longer than an activity or service
+    public NoteRepository(Application application){
+        NoteDatabase database = NoteDatabase.getInstance(application); // the the current instance of the NoteDatabase
+        noteDao = database.noteDao(); // Room auto generate all the necessary codes for this abstract class
+
+        allNotes = noteDao.getAllNotes(); //get all notes
+    }
+
+    //------------------API's that exposed to the outside-------------------------//
+    public void insert(NoteEntity note) {
+        new InsertNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void update(NoteEntity note) {
+        new UpdateNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void delete(NoteEntity note) {
+        new DeleteNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void deleteAllNotes() {
+        new DeleteAllNotesAsyncTask(noteDao).execute();
+    }
+
+    //NOTE : LiveData will execute all the db operations in the background thread
+    public LiveData<List<NoteEntity>> getAllNotes() {
+        return allNotes;
+    }
+
+    //--------------------Background tasks--------------------------//
+
+    private static class InsertNoteAsyncTask extends AsyncTask<NoteEntity,Void,Void>{
+        private NoteDao noteDao; // Required to make Db operations
+
+        private InsertNoteAsyncTask(NoteDao noteDao) { // Make a constructor to pass the values to a static class
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(NoteEntity... noteEntities) {
+            noteDao.insert(noteEntities [0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<NoteEntity, Void, Void> {
+        private NoteDao noteDao;
+
+        private UpdateNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(NoteEntity... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<NoteEntity, Void, Void> {
+        private NoteDao noteDao;
+
+        private DeleteNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(NoteEntity... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesAsyncTask extends AsyncTask<Void, Void, Void> { //NOTE : we don't wanna pass our Note entity
+        private NoteDao noteDao;
+
+        private DeleteAllNotesAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
+            return null;
+        }
+    }
+}
